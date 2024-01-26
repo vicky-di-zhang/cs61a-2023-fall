bd6f590aba0a392e021b392583a719b17a9a900f
zhangdi0531 <zhangdi0531ch@gmail.com>
2024-01-26 22:56:53 +0100

    lab03

---
+++ lab/lab03/lab03.py
@@ -0,0 +1,170 @@
+from operator import add, mul
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+
+def ordered_digits(x):
+    """Return True if the (base 10) digits of X>0 are in non-decreasing
+    order, and False otherwise.
+
+    >>> ordered_digits(5)
+    True
+    >>> ordered_digits(11)
+    True
+    >>> ordered_digits(127)
+    True
+    >>> ordered_digits(1357)
+    True
+    >>> ordered_digits(21)
+    False
+    >>> result = ordered_digits(1375) # Return, don't print
+    >>> result
+    False
+
+    """
+    while x>0:
+        right , left , x = x%10 , (x//10)%10 , x//10
+        if right < left:
+            return False
+    return True
+        
+        
+
+
+def get_k_run_starter(n, k):
+    """Returns the 0th digit of the kth increasing run within n.
+    >>> get_k_run_starter(123444345, 0) # example from description
+    3
+    >>> get_k_run_starter(123444345, 1)
+    4
+    >>> get_k_run_starter(123444345, 2)
+    4
+    >>> get_k_run_starter(123444345, 3)
+    1
+    >>> get_k_run_starter(123412341234, 1)
+    1
+    >>> get_k_run_starter(1234234534564567, 0)
+    4
+    >>> get_k_run_starter(1234234534564567, 1)
+    3
+    >>> get_k_run_starter(1234234534564567, 2)
+    2
+    """
+    i = 0
+    final = None
+    while n > 0 and i < k+1:
+        while n > 10 and n % 10 > (n // 10) % 10:
+            n //= 10
+        final = n % 10
+        i = i + 1
+        n //= 10
+    return final
+
+
+def nearest_two(x):
+    """Return the power of two that is nearest to x.
+
+    >>> nearest_two(8)    # 2 * 2 * 2 is 8
+    8.0
+    >>> nearest_two(11.5) # 11.5 is closer to 8 than 16
+    8.0
+    >>> nearest_two(14)   # 14 is closer to 16 than 8
+    16.0
+    >>> nearest_two(2015)
+    2048.0
+    >>> nearest_two(.1)
+    0.125
+    >>> nearest_two(0.75) # Tie between 1/2 and 1
+    1.0
+    >>> nearest_two(1.5)  # Tie between 1 and 2
+    2.0
+
+    """
+    power_of_two = 1.0
+    "*** YOUR CODE HERE ***"
+    return power_of_two
+
+
+def make_repeater(func, n):
+    """Returns the function that computes the nth application of func.
+
+    >>> add_three = make_repeater(increment, 3)
+    >>> add_three(5)
+    8
+    >>> make_repeater(triple, 5)(1) # 3 * 3 * 3 * 3 * 3 * 1
+    243
+    >>> make_repeater(square, 2)(5) # square(square(5))
+    625
+    >>> make_repeater(square, 4)(5) # square(square(square(square(5))))
+    152587890625
+    >>> make_repeater(square, 0)(5) # Yes, it makes sense to apply the function zero times!
+    5
+    """
+    "*** YOUR CODE HERE ***"
+
+def composer(func1, func2):
+    """Returns a function f, such that f(x) = func1(func2(x))."""
+    def f(x):
+        return func1(func2(x))
+    return f
+
+def apply_twice(func):
+    """Returns a function that applies func twice.
+
+    func -- a function that takes one argument
+
+    >>> apply_twice(square)(2)
+    16
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def div_by_primes_under(n):
+    """
+    >>> div_by_primes_under(10)(11)
+    False
+    >>> div_by_primes_under(10)(121)
+    False
+    >>> div_by_primes_under(10)(12)
+    True
+    >>> div_by_primes_under(5)(1)
+    False
+    """
+    checker = lambda x: False
+    i = ____________________________
+    while ____________________________:
+        if not checker(i):
+            checker = ____________________________
+        i = ____________________________
+    return ____________________________
+
+def div_by_primes_under_no_lambda(n):
+    """
+    >>> div_by_primes_under_no_lambda(10)(11)
+    False
+    >>> div_by_primes_under_no_lambda(10)(121)
+    False
+    >>> div_by_primes_under_no_lambda(10)(12)
+    True
+    >>> div_by_primes_under_no_lambda(5)(1)
+    False
+    """
+    def checker(x):
+        return False
+    i = ____________________________
+    while ____________________________:
+        if not checker(i):
+            def outer(____________________________):
+                def inner(____________________________):
+                    return ____________________________
+                return ____________________________
+            checker = ____________________________
+        i = ____________________________
+    return ____________________________
+

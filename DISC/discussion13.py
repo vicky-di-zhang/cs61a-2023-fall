e260c971a3a72b3d25a7d775bb0537e7d1931a84
zhangdi0531 <zhangdi0531ch@gmail.com>
2024-02-26 10:39:21 +0100

    done

---
+++ DISC/discussion13.py
@@ -0,0 +1,262 @@
+class Link:
+
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
+
+
+def insert_into_all(item, nested_list):
+    """Return a new list consisting of all the lists in nested_list,
+    but with item added to the front of each. You can assume that
+     nested_list is a list of lists.
+
+    >>> nl = [[], [1, 2], [3]]
+    >>> insert_into_all(0, nl)
+    [[0], [0, 1, 2], [0, 3]]
+    """
+    return list(map(lambda x: [item] + x, nested_list))
+
+def subseqs(s):
+    """Return a nested list (a list of lists) of all subsequences of S.
+    The subsequences can appear in any order. You can assume S is a list.
+
+    >>> seqs = subseqs([1, 2, 3])
+    >>> sorted(seqs)
+    [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
+    >>> subseqs([])
+    [[]]
+    """
+    if not s:
+        return [[]]
+    else:
+        first, rest = s[0], s[1:]
+        return insert_into_all(first, subseqs(rest)) + subseqs(rest)
+    
+    
+def long_paths(tree, n):
+    """Return a list of all paths in tree with length at least n.
+
+    >>> t = Tree(3, [Tree(4), Tree(4), Tree(5)])
+    >>> left = Tree(1, [Tree(2), t])
+    >>> mid = Tree(6, [Tree(7, [Tree(8)]), Tree(9)])
+    >>> right = Tree(11, [Tree(12, [Tree(13, [Tree(14)])])])
+    >>> whole = Tree(0, [left, Tree(13), mid, right])
+    >>> for path in long_paths(whole, 2):
+    ...     print(path)
+    ...
+    <0 1 2>
+    <0 1 3 4>
+    <0 1 3 4>
+    <0 1 3 5>
+    <0 6 7 8>
+    <0 6 9>
+    <0 11 12 13 14>
+    >>> for path in long_paths(whole, 3):
+    ...     print(path)
+    ...
+    <0 1 3 4>
+    <0 1 3 4>
+    <0 1 3 5>
+    <0 6 7 8>
+    <0 11 12 13 14>
+    >>> long_paths(whole, 4)
+    [Link(0, Link(11, Link(12, Link(13, Link(14)))))]
+    """
+    def helper(tree, path):
+        if not tree.branches:  # Leaf node
+            if len(path) >= n:
+                return [path]
+            else:
+                return []
+        else:
+            paths = []
+            for branch in tree.branches:
+                paths.extend(helper(branch, path + [branch.label]))
+            return paths
+
+    return helper(tree, [tree.label])
+
+
+
+def deep_len(lnk):
+    """ Returns the deep length of a possibly deep linked list.
+
+    >>> deep_len(Link(1, Link(2, Link(3))))
+    3
+    >>> deep_len(Link(Link(1, Link(2)), Link(3, Link(4))))
+    4
+    >>> levels = Link(Link(Link(1, Link(2)), \
+            Link(3)), Link(Link(4), Link(5)))
+    >>> print(levels)
+    <<<1 2> 3> <4> 5>
+    >>> deep_len(levels)
+    5
+    """
+    if lnk is Link.empty:
+        return 0
+    elif lnk.rest is Link.empty:
+        return 1
+    else:
+        return deep_len(lnk.first) + deep_len(lnk.rest)
+
+
+def powers(n, k):
+    """Yield all powers of k whose digits appear in order in n.
+
+    >>> sorted(powers(12345, 5))
+    [1, 5, 25, 125]
+    >>> sorted(powers(54321, 5))  # 25 and 125 are not in order
+    [1, 5]
+    >>> sorted(powers(2493, 3))
+    [3, 9, 243]
+    >>> sorted(powers(2493, 2))
+    [2, 4]
+    >>> sorted(powers(164352, 2))
+    [1, 2, 4, 16, 32, 64]
+    """
+    is_power = lambda base, s: s == 1 or (s % base == 0 and is_power(base, s // base))
+    curry2 = lambda f: lambda x: lambda y: f(x, y)
+    def build(seed):
+        """Yield all non-negative integers whose digits appear in order in seed.
+        0 is yielded because 0 has no digits, so all its digits are in seed.
+        """
+        if seed == 0:
+            yield 0
+        else:
+            for x in build(seed // 10):
+                yield x
+                yield 10*x + seed % 10
+    yield from filter(curry2(is_power)(k), build(n))
+
